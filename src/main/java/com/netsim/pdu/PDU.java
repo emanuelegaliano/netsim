747c90f4dafc0b049639670107cd68bc3f2224f3
armedfalconer <ema018gal@gmail.com>
2025-05-20 15:05:14 +0200

    package name change of packet to pdu

--- src/main/java/com/netsim/pdu/PDU.java
+++ src/main/java/com/netsim/pdu/PDU.java
@@ -1,18 +1,18 @@
 package com.netsim.packet;
 
-public interface Packet {
+public interface PDU {
 
     /**
      * Encapsulates an upper-layer packet inside this packet.
      * @param payload the higher-level packet to embed
      */
-    void encapsulate(Packet payload);
+    void encapsulate(PDU payload);
 
     /**
      * Decapsulates and returns the upper-layer packet contained in this one.
      * @return the payload, or null if none is present
      */
-    Packet decapsulate();
+    PDU decapsulate();
 
     @Override
     String toString();
